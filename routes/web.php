9bab9a35b0481f448afdd92fdb22c8b58d27f5de
Maria Laura Nieto <nm.laura01@gmail.com>
2021-11-30 16:53:12 -0300

    Arreglo index redirect

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }else{
+        return redirect()->route('login');
+    }
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
